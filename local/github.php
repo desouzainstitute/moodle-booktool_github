bd967bcf764547041463bb1b5f3f655fc4bbfa73
[Carlos ArceLopera] <[carlosarceloperaccatalyst-ca.net]>
2024-01-24 09:29:42 -0500

    Fixed all errors and warnings in local/github.php

--- local/github.php
+++ local/github.php
@@ -1,9 +1,23 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+defined('MOODLE_INTERNAL') || die;
 require_once( __DIR__ . '/client/client/GitHubClient.php' );
 
-
-listCommits();
+listcommits();
 die;
 
 $owner = 'djplaner';
@@ -15,49 +29,45 @@ $password = 'n3tmask3r';
 $client = new GitHubClient();
 $client->setDebug( true );
 $client->setCredentials( $username, $password );
-	
-$sha = getSha( $client, $owner, $repo, $path );
-	
+
+$sha = getsha( $client, $owner, $repo, $path );
+
 print "SHAR is " . $sha . "\n";
 
 $statuses = $client->repos->statuses->listStatusesForSpecificRef( $owner, $repo, $sha );
-	
+
 echo "Num statuses is " . count( $statuses ) . "\n";
-foreach ( $statuses as $status ) {
+foreach ($statuses as $status) {
     echo get_class( $status ) . "\n";
-    print_r( $status );
+    var_dump( $status );
 }
 
-#print "content is " . base64_decode( $response->getContent() );
+function getsha( $client, $owner, $repo, $path) {
 
-#print "name is " . $response->getName();
-
-function getSha( $client, $owner, $repo, $path) {
-
-    $data = array();
+    $data = [];
     $response = $client->request( "/repos/$owner/$repo/contents/$path", 'GET', $data, 200, 'GitHubReadmeContent'   );
 
-    return $response->getSha();
+    return $response->getsha();
 }
 
-#-- display the content of a file
-function getContent() {
+// Display the content of a file.
+function getcontent() {
     $owner = 'djplaner';
     $repo = 'edc3100';
     $path = 'Who_are_you.html';
 
     $client = new GitHubClient();
 
-    $data = array();
+    $data = [];
     $response = $client->request( "/repos/$owner/$repo/contents/$path", 'GET', $data, 200, 'GitHubReadmeContent'   );
 
-    print "content is " . base64_decode( $response->getContent() );
+    print "content is " . base64_decode( $response->getcontent() );
 
     print "name is " . $response->getName();
 }
 
-# get a list of commits
-function listCommits() {
+// Get a list of commits.
+function listcommits() {
 
     $owner = 'djplaner';
     $repo = 'edc3100';
@@ -66,74 +76,70 @@ function listCommits() {
     $client = new GitHubClient();
     $client->setDebug( true );
 
-    $data = array();
+    $data = [];
     $data['path'] = $path;
 
     $before = memory_get_usage();
-    $commits = $client->repos->commits->listCommitsOnRepository( 
+    $commits = $client->repos->commits->listCommitsOnRepository(
                                 $owner, $repo, null, $path );
     $after = memory_get_usage();
 
-print_r($commits);
+    var_dump($commits);
     echo "Count: " . count($commits) . "\n";
-    foreach($commits as $commit)
-    {
-        /* @var $commit GitHubCommit */
-      echo get_class($commit) . " - Sha: " . $commit->getSha() . "\n";
-      $theCommit = $commit->getAuthor();
-      print_r( $theCommit);
+    foreach ($commits as $commit) {
+        echo get_class($commit) . " - Sha: " . $commit->getsha() . "\n";
+        $thecommit = $commit->getAuthor();
+        var_dump( $thecommit);
     }
 
     echo "size is " . convert( $after - $before ) . "\n";
 }
 
-function convert($size)
-{
-    $unit=array('b','kb','mb','gb','tb','pb');
-    return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+function convert($size) {
+    $unit = ['b', 'kb', 'mb', 'gb', 'tb', 'pb'];
+    return @round($size / pow(1024, ($i = floor(log($size, 1024)))), 2).' '.$unit[$i];
 }
 
-function createFile() {
-	$owner = 'djplaner';
-	$repo = 'edc3100';
-	$path = 'A_2nd_new_file.html';
-	$username = 'djplaner';
-	$password = 'n3tmask3r';
-	
-	$content = "This will be the content in the second file. The 1st time";
-	
-	$client = new GitHubClient();
-	$client->setDebug( true );
-	$client->setCredentials( $username, $password );
-	
-	$data = array();
-	$data['message'] = 'First time creating a file';
-	$data['content'] = base64_encode( $content );
-	
-	
-	$response = $client->request( "/repos/$owner/$repo/contents/$path", 'PUT', $data, 201, 'GitHubReadmeContent'   );
-	
-    print_r( $response );
+function createfile() {
+    $owner = 'djplaner';
+    $repo = 'edc3100';
+    $path = 'A_2nd_new_file.html';
+    $username = 'djplaner';
+    $password = 'n3tmask3r';
+
+    $content = "This will be the content in the second file. The 1st time";
+
+    $client = new GitHubClient();
+    $client->setDebug( true );
+    $client->setCredentials( $username, $password );
+
+    $data = [];
+    $data['message'] = 'First time creating a file';
+    $data['content'] = base64_encode( $content );
+
+    $response = $client->request( "/repos/$owner/$repo/contents/$path", 'PUT', $data, 201, 'GitHubReadmeContent');
+
+    var_dump( $response );
 }
 
-function updateFile() {
-	$content = "This will be the content in the second file. The 4th time";
-	
-	$client = new GitHubClient();
-	#$client->setDebug( true );
-	$client->setCredentials( $username, $password );
-	
-	$sha = getSha( $client, $owner, $repo, $path );
-	
-	print "shar is $sha\n\n";
-	$data = array();
-	$data['message'] = 'First time creating a file - Update 2';
-	$data['content'] = base64_encode( $content );
-	$data['sha'] = $sha;
-	$data['committer'] = array( 'name' => 'David Jones', 
-	                            'email' => 'davidthomjones@gmail.com' );
-	
-	$response = $client->request( "/repos/$owner/$repo/contents/$path", 'PUT', $data, 200, 'GitHubReadmeContent'   );
-	
-	print_r( $response );
+function updatefile() {
+    $content = "This will be the content in the second file. The 4th time";
+
+    $client = new GitHubClient();
+
+    $client->setCredentials( $username, $password );
+
+    $sha = getsha( $client, $owner, $repo, $path );
+
+    print "shar is $sha\n\n";
+    $data = [];
+    $data['message'] = 'First time creating a file - Update 2';
+    $data['content'] = base64_encode( $content );
+    $data['sha'] = $sha;
+    $data['committer'] = ['name' => 'David Jones',
+                                'email' => 'davidthomjones@gmail.com' ];
+
+    $response = $client->request( "/repos/$owner/$repo/contents/$path", 'PUT', $data, 200, 'GitHubReadmeContent'   );
+
+    var_dump( $response );
 }
